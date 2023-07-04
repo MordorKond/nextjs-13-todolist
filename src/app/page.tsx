d35c7366a9252ab8b68139423e79030290062f60
Ivaylo Nakov <i.nakov18@gmail.com>
2023-07-05 00:58:32 +0200

    Home page stayling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import Link from "next/link"
 
 export default function Home() {
   return (
-  <h1>hi</h1>
+   <> 
+       <header className="justify-between flex border items-center mb-4">
+           <h1 className="text-2xl">Todos</h1> 
+           <Link className="border rounded px-2 py-1 hover:bg-slate-700 focus-within:bg-slate-700" href={'/new'}>
+               New 
+           </Link>
+       </header>
+       <ul className="p-4"></ul>
+
+   </>
   )
 }
