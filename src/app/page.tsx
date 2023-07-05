4fffe0f9fe19a934236c127c0a9eff29c271d1d7
Ivaylo Nakov <i.nakov18@gmail.com>
2023-07-05 02:53:31 +0200

    TodoItem added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+import { prisma } from "@/db"
 import Link from "next/link"
 
-export default function Home() {
+const getTodos = () => prisma.todo.findMany()
+
+export default async function Home() {
+  const todos = await getTodos() 
+  // await prisma.todo.create({data:{title:'todoggo',complete:false}})
+
   return (
    <> 
        <header className="justify-between flex border items-center mb-4">
@@ -9,8 +15,23 @@ export default function Home() {
                New 
            </Link>
        </header>
-       <ul className="p-4"></ul>
+       <ul className="p-4">
+         {todos.map(todo=>{
+                     return <TodoItem key={todo.id} {...todo} />
+                 })}
+       </ul>
 
    </>
   )
+}
+
+const TodoItem = ({id, title, complete}:{id:string, title:string, complete:boolean}) => {
+    return (
+           <>
+               <li className="flex gap-1 ">
+                 <input type="checkbox" id={id} className="cursor-pointer peer" />
+                 <label htmlFor={id} className="peer-checked:line-through cursor-pointer peer-checked:text-slate-500">{title}</label>
+               </li>
+           </>
+           )
 }
